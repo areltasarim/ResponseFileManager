f7bbd6f1b5673ddf1ebd79dbcad68eafad47e4b5
areltasarim <101069343+areltasarim@users.noreply.github.com>
2022-03-09 03:52:11 +0300

    File Manager

---
+++ WebApplication3/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.Extensions.FileProviders;
+using ResponsiveFileManager;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Adds a default in-memory implementation of IDistributedCache.
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+app.UseSession();
+
+var rfmOptions = new ResponsiveFileManagerOptions();
+app.Configuration.GetSection("ResponsiveFileManagerOptions").Bind(rfmOptions);
+
+app.UseDefaultFiles();
+app.UseStaticFiles(); // shortcut for HostEnvironment.WebRootFileProvider
+app.UseStaticFiles(new StaticFileOptions
+{
+    RequestPath = new PathString("/filemanager"),
+    FileProvider = new PhysicalFileProvider(Path.GetFullPath(Path.Combine(Assembly.GetEntryAssembly().Location, "../filemanager"))),
+});
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
